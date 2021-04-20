fdb03c405fba8479b8dcedeeb591efe5736a5c66
Nao-Y1996 <60519909+Nao-Y1996@users.noreply.github.com>
2021-04-21 07:03:13 +0900

    initial commit <Before 1st makemigrations>

---
+++ users/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
+
+from phonenumber_field.modelfields import PhoneNumberField
+
+# Create your models here.
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    username = models.CharField(
+        verbose_name='Uername',
+        max_length=30,
+        # validators=[username_validator],
+        # error_messages={
+        #     'unique': _("A user with that username already exists.")
+    )
+    phone_number = PhoneNumberField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email' # パスワードと何で認証するかを決める　ここではパスワードとemail
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, phone_number, username, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+        def create_superuser(self, email, phone_number, username, password=None):
+            """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            username=username,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+        
